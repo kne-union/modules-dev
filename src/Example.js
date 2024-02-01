18d77475f1518ea74cfefb6a5caf9ee5daf2f7cf
zhipenglin <sunandmoon001@163.com>
2024-02-01 14:50:39 +0800

    修改Example

--- src/Example.js
+++ src/Example.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useParams, Navigate} from "react-router-dom";
 import ExamplePage from './ExamplePage';
+import ensureSlash from '@kne/ensure-slash';
 
-const Example = ({baseUrl, readme}) => {
+const Example = ({baseUrl, readme, pageProps}) => {
     const {id} = useParams();
     const current = id || Object.keys(readme)[0];
     const data = readme[current];
-    return <ExamplePage data={data} current={current} items={Object.keys(readme).map((name) => {
+
+    if (!readme[current]) {
+        return <Navigate to={`${ensureSlash(baseUrl, true)}${Object.keys(readme)[0]}`} replace/>
+    }
+
+    return <ExamplePage pageProps={pageProps} data={data} current={current} items={Object.keys(readme).map((name) => {
         return {
-            label: name, key: name, path: `${baseUrl}` + name
+            label: name, key: name, path: `${ensureSlash(baseUrl, true)}` + name
         };
     })}/>
 };

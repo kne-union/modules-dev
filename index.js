8cdc658c0cde25f23d5f90bd4f59349b565bcdec
zhipenglin <sunandmoon001@163.com>
2024-02-02 17:50:07 +0800

    支持lib类型项目构建

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const CracoReadmePlugin = require('./lib/craco-readme-plugin');
+const CracoReadmeLibPlugin = require('./lib/craco-readme-lib-plugin');
 const moduleFederationConfig = require('./lib/modulefederation.config');
 const env = require('./lib/env');
 
-module.exports = {CracoReadmePlugin, moduleFederationConfig, env};
+module.exports = {CracoReadmePlugin, CracoReadmeLibPlugin, moduleFederationConfig, env};

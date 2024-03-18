4f8d2c25dafa169ecd8def5b98a542a916c61738
zhipenglin <sunandmoon001@163.com>
2024-03-18 11:50:38 +0800

    添加readme数据远程加载

--- src/Example.js
+++ src/Example.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useParams, Navigate} from "react-router-dom";
 import ExamplePage from './ExamplePage';
 import ensureSlash from '@kne/ensure-slash';
+import Fetch from '@kne/react-fetch';
 
 const Example = ({baseUrl, readme, pageProps}) => {
     const {id: current} = useParams();
@@ -11,11 +12,20 @@ const Example = ({baseUrl, readme, pageProps}) => {
         return <Navigate to={`${ensureSlash(baseUrl, true)}${Object.keys(readme)[0]}`} replace/>
     }
 
-    return <ExamplePage pageProps={pageProps} data={data} current={current} items={Object.keys(readme).map((name) => {
-        return {
-            label: name, key: name, path: `${ensureSlash(baseUrl, true)}` + name
-        };
-    })}/>
+    const renderExamplePage = ({data}) => <ExamplePage pageProps={pageProps} data={data} current={current}
+                                                       items={Object.keys(readme).map((name) => {
+                                                           return {
+                                                               label: name,
+                                                               key: name,
+                                                               path: `${ensureSlash(baseUrl, true)}` + name
+                                                           };
+                                                       })}/>
+
+    if (data && data.hasOwnProperty('loader') || data.hasOwnProperty('url')) {
+        return <Fetch {...Object.assign({}, data)} render={renderExamplePage}/>
+    }
+
+    return renderExamplePage({data});
 };
 
 export default Example;

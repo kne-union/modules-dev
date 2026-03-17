39eea996b610e3b1eaf0b174d214a05a9b460cc3
Linzp <sunandmoon001@163.com>
2026-03-17 11:32:39 +0800

    修改预览页面

--- src/Highlight.js
+++ src/Highlight.js
@@ -11,8 +11,24 @@ const Highlight = ({html, ...props}) => {
         ref.current.querySelectorAll('pre code').forEach((el) => {
             hljs.highlightElement(el);
         });
+
     }, [html]);
-    return <div {...props} ref={ref} dangerouslySetInnerHTML={{__html: html}}/>
+
+    // 给所有 table 元素外面包裹一层 div
+    const wrapTableElements = (htmlString) => {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = htmlString;
+        const tables = tempDiv.querySelectorAll('table');
+        tables.forEach(table => {
+            const wrapper = document.createElement('div');
+            wrapper.className = 'table-content';
+            table.parentNode.insertBefore(wrapper, table);
+            wrapper.appendChild(table);
+        });
+        return tempDiv.innerHTML;
+    };
+
+    return <div {...props} ref={ref} dangerouslySetInnerHTML={{__html: wrapTableElements(html)}}/>
 };
 
 export default Highlight;
